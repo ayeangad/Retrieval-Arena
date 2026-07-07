1ae671542a02fa2b9c632c87c7415773efbf7d88
Angad <angadsankhla09@gmail.com>
2026-07-08 01:19:45 +0530

    initial commit

---
+++ src/ingestion/chunkers/fixed-size.ts
@@ -0,0 +1,38 @@
+import type { Chunk, Chunker, Document } from "../../types"
+
+const strategy = "fixed-size"
+export class FixedSizeChunker implements Chunker {
+  readonly name: string
+  constructor(private chunkSize: number, private overlap: number) {
+    this.name = `fixed-size-${chunkSize}`
+  }
+  chunk(doc: Document): Chunk[] {
+
+    let currentStart = 0
+    const chunks: Chunk[] = []
+    for (currentStart; currentStart < doc.content.length; currentStart += this.chunkSize - this.overlap) {
+      let currentEnd = Math.min(currentStart + this.chunkSize, doc.content.length)
+      const content = doc.content.slice(currentStart, currentEnd)
+
+      const idInput = doc.id + String(currentStart) + String(currentEnd) + strategy
+      const id = String(Bun.hash(String(idInput)))
+
+      const chunk: Chunk = {
+        id: id,
+        documentId: doc.id,
+        content,
+        strategy,
+        charStart: currentStart,
+        charEnd: currentEnd,
+        tokenCount: 0
+      }
+      chunks.push(chunk)
+    }
+
+    return chunks
+  }
+}
+
+
+
+
---
+++ src/types.ts
@@ -0,0 +1,23 @@
+export interface Document {
+  id: string;
+  source: string;
+  content: string;
+}
+
+export interface Chunk {
+  id: string;
+  documentId: string;
+  content: string;
+  strategy: string;
+  charStart: number;
+  charEnd: number;
+  tokenCount: number;
+}
+
+export interface Chunker {
+  readonly name: string;
+  chunk(doc: Document): Chunk[]
+}
+
+
+
