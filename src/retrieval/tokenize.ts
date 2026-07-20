eaa948e97c16c9d6ceb126b6cf2fd07fffaea09e
Angad <angadsankhla09@gmail.com>
2026-07-20 23:05:01 +0530

    add stop words to improve bm25 performance

--- src/retrieval/tokenize.ts
+++ src/retrieval/tokenize.ts
@@ -1,3 +1,11 @@
+const STOP_WORDS = new Set([
+  "a", "an", "and", "are", "as", "at", "be", "but", "by",
+  "for", "if", "in", "into", "is", "it",
+  "no", "not", "of", "on", "or", "such",
+  "that", "the", "their", "then", "there", "these",
+  "they", "this", "to", "was", "will", "with",
+  "what", "would", "happen", "my", "configured", "through"
+]);
 
 export function tokenize(text: string): string[] {
   const lowerCase = text.toLowerCase()
@@ -6,7 +14,9 @@ export function tokenize(text: string): string[] {
   const cleanUp = splitText.map((a) => a.replace(/[^a-zA-Z0-9]/g, ""))
   const freeupSpace = cleanUp.filter(i => i.length !== 0)
 
-  return freeupSpace
+  const stopWords = freeupSpace.filter(token => token.length > 0 && !STOP_WORDS.has(token));
+
+  return stopWords
 }
 
 
